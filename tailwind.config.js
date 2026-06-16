f5a342eb1c1894be171f93cebbfc5ecfecebc418
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-06-17 01:33:32 +0700

    add a tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./**/templates/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            display: ['Sora', 'sans-serif'],
+            body: ['DM Sans', 'sans-serif'],
+        },
+        colors: {
+            surface: '#FAFAFA',
+            ink: '#0F0F0F',
+            muted: '#999999',
+            faint: '#E8E8E8',
+            subtle: '#F3F3F3',
+        }
+    }
   },
   plugins: [],
 }
